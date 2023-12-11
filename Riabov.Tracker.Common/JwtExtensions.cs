bf909b239e2531cf379820e6075efbbc03f7dd6a
Riabov.AA <riabov.aa@parmalogica.ru>
2023-12-11 14:12:12 +0500

    addJwtExtensions

---
+++ Riabov.Tracker.Common/JwtExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Riabov.Tracker.Common;
+
+public static class JwtExtensions
+{
+    public const string SecurityKey = "super secret key1";
+    
+    public static void AddJwtAuthentication(this IServiceCollection services)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecurityKey));
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(
+                opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = key,
+                        ValidateAudience = false,
+                        ValidateIssuer = false,
+                        RequireExpirationTime = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+    }
+}
