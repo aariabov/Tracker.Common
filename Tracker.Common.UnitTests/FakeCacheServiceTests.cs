737d3cc4201b61c597b6ed22a741be6e7a9cb7d8
Riabov.AA <riabov.aa@parmalogica.ru>
2024-04-11 03:22:08 +0500

    feat: add cache service #787835

---
+++ Tracker.Common.UnitTests/FakeCacheServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Riabov.Tracker.Common.Cache;
+
+namespace Tracker.Common.UnitTests;
+
+public class FakeCacheServiceTests
+{
+    [Fact]
+    public async Task fake_cache()
+    {
+        var cacheValue = 1;
+        var sut = new FakeCacheService();
+
+        var val = await sut.GetAsync<int?>("key");
+        Assert.Null(val);
+
+        await sut.SetAsync("key", cacheValue, TimeSpan.FromSeconds(1));
+        val = await sut.GetAsync<int?>("key");
+        Assert.Null(val);
+    }
+}
